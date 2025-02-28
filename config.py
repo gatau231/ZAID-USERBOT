9821910323b734a3eab8542d0439540c2a0a05f0
gatau231 <98986794+gatau231@users.noreply.github.com>
2025-02-28 14:45:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "26346806")) #optional
+API_HASH = getenv("API_HASH", "469ac547ed8b97a245d02409c759fefc") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7937406397").split()))
+OWNER_ID = int(getenv("7937406397"))
 MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7583204643:AAFh4Nau0mooRT4RJU3oVjaZfoGhFKK5Brg")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+GIT_TOKEN = getenv("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") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
